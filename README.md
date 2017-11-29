148dd4cc7e4cbb3745d8dc92ee2cb104c2516fb5
Kip Gebhardt <kgebhardt@lucidg.com>
2017-11-29 14:42:53 -0800

    Fixing some issues. Adding building detail page. Completing documentation

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-## Example BuildingOS Oauth2 Client
-A simple webapp that shows information from BuildingOS
+## Example BuildingOS Oauth2 Application
+An exampled web application that retrieves building data from BuildingOS via Oauth2 authentication.
+
+## Prerequisites
+- A BuildingOS user account
+- Some buildings in your organization
+- An API Client of type "Authorization code" (See https://buildingos.com/developers/)
+
+## Installation
+```
+git clone https://github.com/luciddg/nodejs-api-example.git
+cd nodejs-api-example
+npm install
+```
+Create a configuration file:
+```
+cp config_example.json config.json
+```
+Edit that file and paste in your API Client ID and Secret
+
+Start the application:
+```
+npm start
+```
+Point your browser at http://127.0.0.1:3000
 
 In this app you are able to:
 - Login with OAuth2
 - Show list of buildings
-- TODO: Select one from list
-- TODO: View Building with all meters
-- TODO: Each meter should have an the type of resource and the most recent reading. 
+- Select one from list and see a detail view
+
+TODO
+- View Building with all meters
+- Each meter should have an the type of resource and the most recent reading.
--- app.js
+++ app.js
@@ -1,92 +1,94 @@
-var util = require('util');
-var express = require('express');
+const config = require('./config.json'),
+  express = require('express'),
+  logger = require('./lib/logger'),
+  request = require('request'),
+  session = require('express-session'),
+  simpleOauth2 = require('simple-oauth2'),
+  util = require('util');
+
 var app = express();
-
-// Used to make requests to the BOS API
-var request = require('request');
-
-// Expose our public static folder
 app.use(express.static('public'));
 
 // We use Jade for our views
 app.set('view engine', 'jade');
 
 // Setup sessions so we can store the auth token
-var session = require('express-session')
 app.use(session({
   secret: '2HD0mWix5CBNoa8szqqTbZu13IBdk2',
   resave: false,
   saveUninitialized: true
 }))
- 
-// Our custom app configs
-// Contains Oauth2 key/secret
-var config = require('./config.json');
 
-var oauth2 = require('simple-oauth2')({
-  clientID: config.client_id,
-  clientSecret: config.client_secret,
-  site: 'https://api.buildingos.com',
-  authorizationPath: '/o/authorize/',
-  tokenPath: '/o/token/',
-  revocationPath: '/oauth2/revoke/'
-});
- 
-// Authorization uri definition 
-var authorization_uri = oauth2.authCode.authorizeURL({
+const credentials = {
+  client: {
+    id: config.client_id,
+    secret: config.client_secret
+  },
+  auth: {
+    tokenHost: config.api_host,
+    tokenPath: '/o/token/',
+    authorizePath: '/o/authorize/',
+
+  }
+};
+const oauth2 = simpleOauth2.create(credentials);
+
+// Authorization uri definition
+var authorization_uri = oauth2.authorizationCode.authorizeURL({
   redirect_uri: 'http://localhost:3000/callback',
   scope: 'read',
   state: 'simple_state'
 });
- 
-// Initial page redirecting to BuildingOS 
+
+// Initial page redirecting to BuildingOS
 app.get('/auth', function (req, res) {
-    res.redirect(authorization_uri);
+  res.redirect(authorization_uri);
 });
- 
-// Callback service parsing the authorization token and asking for the access token 
+
+// Callback service parsing the authorization token and asking for the access token
 app.get('/callback', function (req, res) {
   var code = req.query.code;
-  console.log('/callback');
-  console.log(req.query);
-  oauth2.authCode.getToken({
+  logger.info('/callback', req.query);
+  oauth2.authorizationCode.getToken({
     code: code,
     redirect_uri: 'http://localhost:3000/callback'
   }, saveToken);
- 
+
   function saveToken(error, result) {
-    console.log('result', util.inspect(result));
-    console.log('error', util.inspect(error));
-    if (error) { 
-      console.log('Access Token Error', error.message); 
-    }
-    else {
+    if (error) {
+      logger.error('Access Token Error:', error.message);
+      res.redirect('/');
+    } else {
       // stash the token in the user session
       var auth = oauth2.accessToken.create(result);
-      console.log('Returned Auth Token', util.inspect(auth.token));
+      logger.info('result', util.inspect(result));
+      logger.info('Returned Auth Token', util.inspect(auth.token));
       req.session.access_token = auth.token.access_token;
       res.redirect('/buildings');
     }
   }
 
 });
- 
+
 // root of the app. Just has a link to kick off the auth process
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
+  res.render('index');
 });
 
 // Lists the buildings for the current user
 app.get('/buildings', function (req, res){
-  console.log('Session Auth Token', req.session.access_token);
-  var buildings = [];
+  if (!req.session.access_token) {
+    res.redirect(authorization_uri);
+  }
+  logger.info('Session Auth Token', req.session.access_token);
   var options = {
-    url: 'https://api.buildingos.com/buildings',
-    headers: {
-      'Authorization': 'Bearer ' + req.session.access_token
-    }
-  };
+      url: config.api_host + '/buildings',
+      headers: {
+        'Authorization': 'Bearer ' + req.session.access_token
+      }
+    };
   request(options, function (error, response, body) {
+    var buildings = [];
     if (!error && response.statusCode == 200) {
       buildings = JSON.parse(body).data;
       res.render('buildings', {title: 'Bulidings', buildings: buildings});
@@ -94,37 +96,31 @@ app.get('/buildings', function (req, res){
   });
 });
 
-// Experiment with Chart.js
-app.get('/chart_demo', function (req, res){
-  res.render('chart_demo', {title: 'Demo'});
+app.get('/buildings/:id', function (req, res){
+  if (!req.session.access_token) {
+    res.redirect(authorization_uri);
+  }
+  logger.info('Session Auth Token', req.session.access_token);
+  const id = req.params.id,
+    options = {
+      url: config.api_host + '/buildings/' + id,
+      headers: {
+        'Authorization': 'Bearer ' + req.session.access_token
+      }
+    };
+  request(options, function (error, response, body) {
+    var data = {};
+    if (!error && response.statusCode == 200) {
+      data = JSON.stringify(JSON.parse(body).data, null, 4);
+      res.render('building_detail', {data: data});
+    }
+  });
 });
 
-// Experiment with generating data readings for charting demo
-app.get('/data', function (req, res){
-  var moment = require('moment');
-  require('moment-range');
-  var start = new Date(2014, 1, 1);
-  var end   = new Date(2015, 1, 1);
-  var timespan = moment.range(start, end);
-  var readings = [];
-
-  timespan.by('months', function(moment) {
-    var ts = moment.format();
-    var vals = {'a':1, 'b': 2, 'c': 3};
-    var ts_reading = {};
-    ts_reading[ts] = vals;
-    readings.push(ts_reading); 
-  }); 
-  
-  res.json({"data": readings});
-});
- 
 // starting up the app and logging the address/port
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(3000, 'localhost', function () {
+  var host = server.address().address,
+    port = server.address().port;
   process.env.DEBUG=true;
-  console.log('Example app listening at http://%s:%s', host, port);
-
+  logger.info('Example app listening at http://%s:%s', host, port);
 });
